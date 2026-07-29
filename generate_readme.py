5f544895669bf7f5544b1a96e1b9ba73e02975a0
Nathan <nathan.domenichini@gmail.com>
2026-07-29 19:21:45 +0200

    update

--- generate_readme.py
+++ generate_readme.py
@@ -57,7 +57,7 @@ def generate_readme():
         '###',
         '',
         '<p align="center">',
-        '<img height="150" src="https://avatars.githubusercontent.com/flash2974" alt="Profile Picture" style="vertical-align: middle;" />',
+        '<img height="195" src="https://avatars.githubusercontent.com/flash2974" alt="Profile Picture" />',
         '<img src="https://github-stats-extended.vercel.app/api?username=flash2974&hide_title=false&hide_rank=false&show_icons=true&include_all_commits=true&count_private=true&disable_animations=false&theme=midnight-purple&locale=en&hide_border=false&custom_title=My%20stats%20" alt="GitHub Stats">',
         '</p>',
         '',
